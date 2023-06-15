f7a59fcad7e792bc86ffc5682ed57d4f006de204
atielijohn8 <atielijohn4@gmail.com>
2023-06-15 16:56:55 -0700

    c pragramming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - C program that prints exactly and that
+ *
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	write(2,
+			"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59;
+			return (10;
+				}
