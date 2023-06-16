ed7c2cc310ef9c5265be9ad6292c875345218ce2
atielijohn8 <atielijohn4@gmail.com>
2023-06-15 17:14:04 -0700

    c pragramming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - C program that prints exactly and that
- *
- * Return: 1 (success)
+ * main - c program that prints aline to std error
+ * Return 1 (Success)
  */
 int main(void)
 {
-	write(2,
-			"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59;
-			return (10;
-				}
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
