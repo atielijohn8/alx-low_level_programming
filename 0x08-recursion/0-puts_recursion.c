41ea07d88c5a34b57f452b73a7ea3d4b7186cac4
atielijohn8 <atielijohn4@gmail.com>
2023-07-05 14:34:10 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,18 @@
 #include "main.h"
+#include <unistd.h>
 /**
- * a program that prints a string.
- * followed by a new line.
- * @s: input
- * Return: 0 (success)
+ * a fuction that prints  strings
+ * followed a new line
+ * @s:input
+ * Return: 0 (Success)
  */
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 {
-	if(*s)
+	if (*s)
 	{
 		_putchar(*s);
-		_puts _recursion(s + !);
+		_puts_recursion(s + 1);
 	}
-	
 	else
-
-		_putchar('\n');
+		_putchar(\'n');
 }
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,2 @@
-Recursion
-this is aprogramming concept in funtions are reapeted until certain condition is a certained
+ - Recursion
+a program that repeats its function
