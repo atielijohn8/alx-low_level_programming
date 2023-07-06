5aae9bc4bfd96bfbef8d819629df1d520c782556
atielijohn8 <atielijohn4@gmail.com>
2023-07-06 14:07:54 -0700

    error fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,7 +13,7 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return(check_palindrome (s, 0, _strlen_recursion(s)));
+	return (check_palindrome(s, 0, _strlen_recursion(s)));
 }
 
 /**
@@ -43,5 +43,5 @@ int check_palindrome(char *s, int i, int len)
 		return (0);
 	if (i >= len)
 		return (1);
-	return(check_palindrome (s, i + 1, len - 1));
+	return (check_palindrome(s, i + 1, len - 1));
 }
