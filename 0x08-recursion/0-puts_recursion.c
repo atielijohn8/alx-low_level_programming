2c1f205b3a6430c7de114d4eab4acfe3ee2d6f87
atielijohn8 <atielijohn4@gmail.com>
2023-07-05 18:46:19 -0700

    string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,13 @@
-#include "main.h"
-/**fuction that pints string in reverse
- *  @s: input
- *  Return 0 (Success)
- */
-void _puts_recursion(char *s)
+nclude "main.h"
+/**
+ *  * function that prints strings .
+ *   * @s:string to be printed.
+ *    */
+void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-
-	else
-		_putchar('\n');
+		if (*s)
+				{
+							_print_rev_recursion(s + 1);
+									_putchar(*s);
+										}
 }
