fd38bfa7854116d63508521f105b1b45d8422557
atielijohn8 <atielijohn4@gmail.com>
2023-09-29 02:09:48 +0300

    bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,10 +12,16 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned int j;
 
 	if (index > 63)
+	{
 		return (-1);
+	}
 
 	j = 1 << index;
-	*n = (*n | j);
+
+	if (*n & j)
+	{
+		*n ^= j;
+	}
 
 	return (1);
 }
