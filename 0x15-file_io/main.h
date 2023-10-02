6b95aceaa595526847cfe7d070ce997edbbf160e
atielijohn8 <atielijohn4@gmail.com>
2023-10-03 02:08:58 +0300

    file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+/*prototype description
+*
+*
+*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
